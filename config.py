5d2c3d0fc534766ec759023cfe8632505e1077fd
Yolecomte <yoann_lecomte31@orange.fr>
2018-12-03 16:33:31 +0100

    add download and start with flash message, improve front end

--- config.py
+++ config.py
@@ -8,6 +8,11 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 UPLOAD_FOLDER = 'media/'
 
+# Tracks types
 TRACKS_TYPES = ['enduro', 'XC', 'DH']
 
+# Allowed extensions to load traces (only gpx supported)
 ALLOWED_EXTENSIONS = ['gpx',]
+
+# Number of traces showed in on page in index.html
+PER_PAGE = 5
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, request, render_template, redirect, url_for
+from flask import Flask, flash, request, render_template, redirect, url_for, Response
 from werkzeug.utils import secure_filename
 from models import db, Traces
 
@@ -7,6 +7,7 @@ import os
 from utils import DB_GeoJson
 import geojson
 import gpxpy
+from gpxpy import gpx as _gpx
 
 
 def allowed_file(filename):
@@ -25,17 +26,26 @@ def index():
 @app.route('/traces/page/')
 @app.route('/traces/page/<int:page>')
 def home(page=1):
-    per_page = 5
+    """
+    Retrieve all the traces and paginate them
+    """
+    per_page = app.config['PER_PAGE']
     traces = Traces.query.paginate(page, per_page, error_out=False)
     return render_template('index.html', traces=traces, home_button=False)
 
 @app.route('/api/traces/')
 def api_traces():
+    """
+    Retrieve all geom traces in db to show on the map 
+    """
     traces = conn.get_table_as_geojson(Traces.__tablename__)
     return geojson.dumps(traces, indent=4, sort_keys=True, default=str)
 
 @app.route('/traces/new/', methods=['POST','GET'])
 def new_trace():
+    """
+    Create a new trace from a user drawing
+    """
     if request.method == 'POST':
         db.session.add(Traces(request.form['name'],
                               request.form['comment'],
@@ -47,10 +57,13 @@ def new_trace():
 
 @app.route('/traces/new/gpx/', methods=['POST','GET'])
 def new_trace_gpx():
+    """
+    Create a new trace from a GPX file
+    """
     if request.method == 'POST':
         print request.files
         if 'gpx_file' not in request.files:
-            flash('No file supplied...')
+            flash('No file supplied...', 'error')
             return redirect(request.url)
         gpx_file = request.files['gpx_file']
         if gpx_file and allowed_file(gpx_file.filename):
@@ -86,21 +99,53 @@ def new_trace_gpx():
 
 @app.route('/traces/delete/<trace_id>')
 def delete_trace(trace_id):
+    """
+    Delete a trace
+    """
     db.session.delete(Traces.query.get(trace_id))
     db.session.commit()
+    flash('The trace has been succesfully deleted!', 'success')
     return redirect(url_for('home'))
 
 @app.route('/traces/<trace_id>')
 def trace(trace_id):
+    """
+    Retrieve single trace datas 
+    """
     trace = Traces.query.get(trace_id)
     return render_template('trace_detail.html',trace=trace, home_button=True)
 
 @app.route('/api/traces/<trace_id>')
 def api_trace(trace_id):
+    """
+    Retrieve the geometry of a single trace to show it on the map
+    """
     trace = conn.get_single_data_as_geojson(Traces.__tablename__, trace_id)
     return geojson.dumps(trace,indent=4, sort_keys=True, default=str)
 
+@app.route('/api/traces/download/<trace_id>')
+def download(trace_id):
+    """
+    Create a GPX file and expose it to the user for downloading
+    """
+    trace = conn.get_single_data_as_geojson(Traces.__tablename__, trace_id)
 
+    gpx = _gpx.GPX()
+        
+    coords = list(geojson.utils.coords(trace[0]))
+
+    gpx_track = _gpx.GPXTrack()
+    gpx.tracks.append(gpx_track)
+    
+    gpx_segment = _gpx.GPXTrackSegment()
+    gpx_track.segments.append(gpx_segment)
+
+    for coord in coords:
+        print coord[0]
+        gpx_segment.points.append(_gpx.GPXTrackPoint(coord[0], coord[1], elevation=0))
+    
+    return Response(gpx.to_xml(), mimetype="text/plain", headers={"Content-Disposition":"attachment;filename=track.gpx"})
+	
 
 if __name__ == '__main__':
     db.init_app(app)
